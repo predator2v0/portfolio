08e3833c57647e03aeb32a33922ea006d4da3663
subhakanta roul <papu.roul99@gmail.com>
2022-06-17 20:54:55 +0530

    src resources for the project

---
+++ src/res/Experiences.js
@@ -0,0 +1,10 @@
+const Experiences = [
+    {
+        designation: "Application Development Associate",
+        company: "Accenture",
+        duration: "June 2021 - Ongoing",
+        location: "Bangalore",
+    },
+];
+
+module.exports = Experiences;
