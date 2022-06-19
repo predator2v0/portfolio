e5bcb8f25111337679e5ea75a3954a8d1faa5066
subhakanta roul <papu.roul99@gmail.com>
2022-06-19 13:13:16 +0530

    add the footer component to the view

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ import Profile from "./components/Profile";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 const Home = () => {
     return (
         <div className='container'>
@@ -11,6 +12,7 @@ const Home = () => {
             <About />
             <Experience />
             <Projects />
+            <Footer />
         </div>
     );
 };
