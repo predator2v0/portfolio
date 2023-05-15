ca7898a1f67e7f227984e1e251c7ada3e92cca3b
subhakanta roul <papu.roul99@gmail.com>
2023-05-15 23:25:45 +0530

    WIP projects section

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -3,7 +3,53 @@ import './projects.scss';
 const projects = () => {
     return (
         <div className='projects container'>
-            this is the projects section
+            <h1>My Projects</h1>
+            <div className="project-container">
+                <div className="project">
+                    <img src="#" alt="" className="project-thumbnail" />
+                    <h3>Project Name</h3>
+                    <p>project description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus modi ab minima veniam nisi voluptate.</p>
+                    <div className="tags">
+                        <p><span>tag1</span><span>tag2</span><span>tag3</span></p>
+                    </div>
+                    <div className="project-links">
+                        <a href="https://github.com/predator2v0/xyz" className="project-link">github</a>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src="#" alt="" className="project-thumbnail" />
+                    <h3>Project Name</h3>
+                    <p>project description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus modi ab minima veniam nisi voluptate.</p>
+                    <div className="tags">
+                        <p><span>tag1</span><span>tag2</span><span>tag3</span></p>
+                    </div>
+                    <div className="project-links">
+                        <a href="https://github.com/predator2v0/xyz" className="project-link">github</a>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src="#" alt="" className="project-thumbnail" />
+                    <h3>Project Name</h3>
+                    <p>project description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus modi ab minima veniam nisi voluptate.</p>
+                    <div className="tags">
+                        <p><span>tag1</span><span>tag2</span><span>tag3</span></p>
+                    </div>
+                    <div className="project-links">
+                        <a href="https://github.com/predator2v0/xyz" className="project-link">github</a>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src="#" alt="" className="project-thumbnail" />
+                    <h3>Project Name</h3>
+                    <p>project description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus modi ab minima veniam nisi voluptate.</p>
+                    <div className="tags">
+                        <p><span>tag1</span><span>tag2</span><span>tag3</span></p>
+                    </div>
+                    <div className="project-links">
+                        <a href="https://github.com/predator2v0/xyz" className="project-link">github</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
