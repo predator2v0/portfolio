ffbbfe161155bceddce06af9919ff738db0d4dd7
subhakanta roul <papu.roul99@gmail.com>
2023-05-28 00:37:57 +0530

    adding about seciton

---
+++ src/components/about/About.jsx
@@ -0,0 +1,22 @@
+import "./about.scss";
+
+const About = () => {
+    return (
+        <section className='about container' id="about">
+            <h1 className='section-title'>About Me</h1>
+            <div className='about-content'>
+                <p>
+                    I am a software developer specializing in web development
+                    using the MERN stack. With a keen eye for detail and a
+                    passion for creating intuitive and responsive web
+                    applications, I strive to deliver top-quality work that
+                    exceeds client expectations. My portfolio showcases a
+                    diverse range of projects that demonstrate my expertise in
+                    building scalable and robust web applications.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default About;
